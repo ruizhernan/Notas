4d1c7b080e449083865ef31a69976ddf9b346cf6
Hernán Ruiz <63321820+ruizhernan@users.noreply.github.com>
2024-01-29 16:49:50 -0300

    Update NotasContext.cs

--- backend/Models/NotasContext.cs
+++ backend/Models/NotasContext.cs
@@ -25,8 +25,8 @@ namespace NotasEnsolvers.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //optionsBuilder.UseSqlServer("Server=DESKTOP-0G4DA0T\\SQLEXPRESS;DataBase=Notas;Integrated Security=True;");
-               optionsBuilder.UseSqlServer("Data Source=SQL5063.site4now.net;Initial Catalog=db_aa4719_notas;User Id=db_aa4719_notas_admin;Password=Notas123");
+             
+             
 
             }
         }
